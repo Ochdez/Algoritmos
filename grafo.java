c0fdc940f44c85f286c6c37b4cbb293a2106c1fa
Octavio <coronach95@gmail.com>
2018-12-13 07:34:07 -0600

    Update grafo.java

--- grafo.java
+++ grafo.java
@@ -1,59 +1,53 @@
 package djsktra;
 import java.util.*;
 
-public class grafo {
+import java.util.Scanner;
+
+public class app {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num_Nodos, num_Enlaces, salida, entrada, peso,k=0, r;
+
+       
+        //Numero de nodos
+        num_Nodos = sc.nextInt();
+
+        //Numero de enlaces
+        num_Enlaces = sc.nextInt();
+
+        grafoD matriz = new grafoD(num_Nodos);
+
+        while(k < num_Enlaces){
+
+            salida = sc.nextInt();
+            entrada = sc.nextInt();
+            peso = sc.nextInt();
+            matriz.addEnlace(salida,entrada,peso);
+            k++;
+        }
+   
+    
+  }
+}
+
+class grafoD {
     public int[][] adjM;
-    public int vertices;
-    
-    public grafo(int vertices){
-        this.vertices = vertices;
-        adjM = new int[vertices][vertices];
-                
+    public int numVertices;
+
+    public grafoD(int numVertices){
+        this.numVertices = numVertices;
+        adjM = new int[numVertices][numVertices];
+
     }
-    
+
     public void addEnlace(int N_inicial, int N_final, int peso){
         adjM[N_inicial][N_final] = peso;
-        
+        adjM[N_final][N_inicial] = peso;
     }
-    
+
     public int getEnlace(int i, int j) {
         return adjM[i][j];
 
     }
-
-    
-
-    public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in);
-        int n=5;
-        grafo M = new grafo(n);
-        M.addEnlace(0,1,5);
-        M.addEnlace(0,3,7);
-        M.addEnlace(1,0,5);
-        M.addEnlace(1,2,3);
-        M.addEnlace(2,1,3);
-        M.addEnlace(2,3,9);
-        M.addEnlace(3,0,7);
-        M.addEnlace(3,2,9);
-        M.addEnlace(3,4,4);
-        M.addEnlace(4,2,3);
-        M.addEnlace(4,3,4);
-        
-        System.out.println("ingrese el numero de enlaces");
-
-         
-       
-        for (int i = 0; i <n; i++){ 
-            System.out.print(i);
-            for (int j=0; j <n; j++){
-                System.out.print(M.getEnlace(i, j));
-                } 
-
-                System.out.println();
-
-            }
- 
-    }
-    
-}
